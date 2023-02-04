cbb95a4e362402dc7a890edd3582586e5922a39a
Sebastian Döll <sebastian@katallaxie.me>
2023-02-04 20:31:34 +0100

    fix: install deps

--- gen.go
+++ gen.go
@@ -1,5 +1,4 @@
 package main
 
-//go:generate echo "Generating..."
 //go:generate protoc -I. --go_out=paths=source_relative:. urn/urn.proto
 //go:generate protoc -I. --go_out=paths=source_relative:. snowflake/snowflake.proto
