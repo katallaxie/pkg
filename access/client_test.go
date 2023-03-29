d6c0a871b888a85ca7f173af6ccf241c2904d60e
Sebastian Döll <sebastian@katallaxie.me>
2023-03-29 19:03:27 +0000

    wip: bench new client

--- access/client_test.go
+++ access/client_test.go
@@ -46,3 +46,44 @@ func TestNewClient(t *testing.T) {
 		assert.True(t, ok)
 	})
 }
+
+func BenchmarkNewClient(b *testing.B) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	p := &Policy{
+		Version: DefaultVersion,
+		Rules: Rules{
+			{
+				ID:     "1",
+				Effect: Allow,
+				Resources: Resources{
+					"urn:cloud:access:eu-central-1:12345678910:root",
+				},
+				Actions: Actions{
+					"access:changePassword",
+				},
+			},
+		},
+	}
+
+	noop := newNoopServer(IdentityBasedMatcher, p)
+
+	b.ReportAllocs()
+
+	WithNoop(ctx, &testing.T{}, noop, func(ctx context.Context, t *testing.T, dial func(context.Context, string) (net.Conn, error)) {
+		conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(dial), grpc.WithTransportCredentials(insecure.NewCredentials()))
+		assert.NoError(t, err)
+
+		defer conn.Close()
+
+		c, err := NewClient(conn)
+		assert.NoError(t, err)
+
+		for i := 0; i < b.N; i++ {
+			ok, err := c.Check(ctx, "urn:cloud:access:eu-central-1:12345678910:root", "urn:cloud:access:eu-central-1:12345678910:root", "access:changePassword")
+			assert.NoError(t, err)
+			assert.True(t, ok)
+		}
+	})
+}
