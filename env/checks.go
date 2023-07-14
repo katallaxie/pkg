5a2dc85b6f0fa4d8a1d35549ebed4276235d4a46
Sebastian Döll <sebastian@katallaxie.me>
2023-07-14 18:41:11 +0000

    chore: add empty folder check

--- env/checks.go
+++ env/checks.go
@@ -2,7 +2,10 @@ package env
 
 import (
 	"context"
+	"fmt"
 	"os/user"
+
+	"github.com/katallaxie/pkg/utils/files"
 )
 
 // HasUser is checking if the current user is available.
@@ -11,6 +14,22 @@ func HasUser() Check {
 		_, err := user.Current()
 		if err != nil {
 			return err
+		}
+
+		return nil
+	}
+}
+
+// IsDirEmpty is checking if the folder is not empty.
+func IsDirEmpty(path string) Check {
+	return func(ctx context.Context) error {
+		empty, err := files.IsDirEmpty(path)
+		if err != nil {
+			return err
+		}
+
+		if !empty {
+			return fmt.Errorf("folder %s is not empty", path)
 		}
 
 		return nil
--- env/checks_test.go
+++ env/checks_test.go
@@ -2,6 +2,8 @@ package env_test
 
 import (
 	"context"
+	"os"
+	"path"
 	"testing"
 
 	"github.com/katallaxie/pkg/env"
@@ -12,4 +14,22 @@ func TestHasUser(t *testing.T) {
 	t.Parallel()
 
 	assert.NoError(t, env.HasUser()(context.Background()))
+}
+
+func TestIsDirEmpty(t *testing.T) {
+	t.Parallel()
+
+	tempDir, err := os.MkdirTemp(os.TempDir(), "empty_test")
+	assert.NoError(t, err)
+
+	defer func() { _ = os.RemoveAll(tempDir) }()
+
+	assert.NoError(t, env.IsDirEmpty(tempDir)(context.Background()))
+
+	f, err := os.Create(path.Join(tempDir, "test.txt"))
+	assert.NoError(t, err)
+
+	f.Close()
+
+	assert.Error(t, env.IsDirEmpty(tempDir)(context.Background()))
 }
