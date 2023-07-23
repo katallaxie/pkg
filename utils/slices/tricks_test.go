7d4871baa502e89f0f1d5b3c8f881d931e4eec74
Sebastian Döll <sebastian@katallaxie.me>
2023-07-23 14:20:04 +0000

    chore: add test delete and cut

--- utils/slices/tricks_test.go
+++ utils/slices/tricks_test.go
@@ -58,3 +58,55 @@ func TestPush(t *testing.T) {
 		})
 	}
 }
+
+func TestCut(t *testing.T) {
+	t.Parallel()
+
+	var tests = []struct {
+		name     string
+		start    int
+		end      int
+		input    []int
+		expected []int
+	}{
+		{
+			name:     "cut from slice",
+			start:    1,
+			end:      2,
+			input:    []int{1, 2, 3},
+			expected: []int{1, 3},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := slices.Cut(tt.start, tt.end, tt.input...)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	t.Parallel()
+
+	var tests = []struct {
+		name     string
+		idx      int
+		input    []int
+		expected []int
+	}{
+		{
+			name:     "delete from slice",
+			idx:      1,
+			input:    []int{1, 2, 3},
+			expected: []int{1, 3},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := slices.Delete(tt.idx, tt.input...)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
