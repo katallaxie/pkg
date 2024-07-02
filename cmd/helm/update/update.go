9f6c56266035d50909a0bbe618c1834aadb34e5e
Sebastian Döll <sebastian@katallaxie.me>
2024-07-02 20:20:25 +0000

    fix: use semver without v

--- cmd/helm/update/update.go
+++ cmd/helm/update/update.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/katallaxie/pkg/logger"
 
@@ -29,10 +30,14 @@ func main() {
 	pflag.StringVar(&f.Version, "version", f.Version, "version")
 	pflag.Parse()
 
+	f.Version = semver.Canonical(f.Version)
+
 	ok := semver.IsValid(f.Version)
 	if !ok {
 		log.Fatal(errors.New("updater: no valid version"))
 	}
+
+	f.Version = strings.TrimPrefix(f.Version, "v")
 
 	meta, err := chartutil.LoadChartfile(f.File)
 	if err != nil {
