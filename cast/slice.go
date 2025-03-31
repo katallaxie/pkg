efc38a8c7787669d5b731652fdf1a54ef39daff2
Sebastian Döll <sebastian@katallaxie.dev>
2025-03-31 11:07:51 +0000

    chore: cast slice

--- cast/slice.go
+++ cast/slice.go
@@ -1,6 +1,6 @@
 package cast
 
-// Slice is a type that represents a slice.
-func Slice[T any](val T) []T {
-	return []T{val}
+// Slice casts any interface to a slice.
+func Slice[T any](val interface{}) []T {
+	return val.([]T)
 }
