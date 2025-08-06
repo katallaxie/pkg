e4e254c2f87ea384f20ceea036319f508ebc903b
Sebastian Döll <sebastian@katallaxie.dev>
2025-08-06 20:20:38 +0000

    fix(test): elements match key value

--- slices/slices_test.go
+++ slices/slices_test.go
@@ -561,7 +561,7 @@ func TestKeyValue(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			actual := slices.KeyValue(tt.input)
-			assert.Equal(t, tt.expected, actual)
+			assert.ElementsMatch(t, tt.expected, actual)
 		})
 	}
 }
